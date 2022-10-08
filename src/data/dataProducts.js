e0ba6355f4ea16649b5012681704202f19dfd44c
Helder Medeiros <hmedeiros211@gmail.com>
2022-10-07 22:24:24 -0300

    feat(data): :card_file_box: example product data

---
+++ src/data/dataProducts.js
@@ -0,0 +1,28 @@
+import { beer, burger, fries, sushi } from '../assets/productImages';
+
+export default [
+  {
+    name: 'Tasty Burger',
+    description: 'Australian bread, meat, cheddar, lettuce and pickles.',
+    price: 32.90,
+    img: burger,
+  },
+  {
+    name: 'Fries',
+    description: 'Tasty portion of crispy fries.',
+    price: 11.00,
+    img: fries,
+  },
+  {
+    name: 'Sushi',
+    description: 'Delights of japanese cuisine.',
+    price: 39.00,
+    img: sushi,
+  },
+  {
+    name: 'Beer',
+    description: 'Try one of the best beers.',
+    price: 8.00,
+    img: beer,
+  },
+];
