a76dae4153c6fa5c779e42b09693cae575b06e2f
Helder Medeiros <hmedeiros211@gmail.com>
2022-10-07 23:51:38 -0300

    refactor(product): :recycle: correctly rendering the products card

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './ProductsCard.css';
 
 function ProductsCard(props) {
   const { name, description, price, img } = props;
   return (
-    <div>
-      <p>{name}</p>
+    <div className="product-card">
+      <h2 className="product-card-title">{name}</h2>
+      <img
+        className="product-card-image"
+        src={ img }
+        alt={ name }
+      />
       <p>{description}</p>
-      <p>{price}</p>
-      <img src={ img } alt={ name } />
+      <h3>{`$${price.toFixed(2)}`}</h3>
+      <div>
+        <button type="button" className="product-card-increment-button">-</button>
+        {` ${'0'} `}
+        <button type="button" className="product-card-increment-button">+</button>
+        <button type="button" className="product-card-add-button">
+          Add
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ProductsCard from '../components/ProductsCard';
 import dataProducts from '../data/dataProducts';
+import './Products.css';
 
 function Products() {
   return (
-    <div>
-      {dataProducts.map((item) => ProductsCard(item))}
+    <div className="products">
+      <section className="products-introduction">
+        <h1>Quality and tasty food </h1>
+        <p>enjoy our week with 20% discount</p>
+      </section>
+      <section className="products-container">
+        {dataProducts.map((item) => ProductsCard(item))}
+      </section>
     </div>
   );
 }
