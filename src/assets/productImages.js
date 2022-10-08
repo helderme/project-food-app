5709bd521af4f4ae3121bae8fe4e279cc464a9b8
Helder Medeiros <hmedeiros211@gmail.com>
2022-10-08 17:50:16 -0300

    chore(assets): :bento: adding svg icons for github and linkedin

--- src/assets/productImages.js
+++ src/assets/productImages.js
@@ -2,5 +2,7 @@ import beer from './beer.png';
 import burger from './burger.png';
 import fries from './fries.png';
 import sushi from './sushi.png';
+import githubIcon from './github-icon.svg';
+import linkedinIcon from './linkedin-icon.svg';
 
-export { beer, burger, fries, sushi };
+export { beer, burger, fries, sushi, githubIcon, linkedinIcon };
