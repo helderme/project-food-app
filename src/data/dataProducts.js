af1f72b6cae00bb2fd028b4113b7fc8379dd1f7e
Helder Medeiros <hmedeiros211@gmail.com>
2022-10-07 23:52:27 -0300

    refactor(data): :recycle: adding id for dataProducts example

--- src/data/dataProducts.js
+++ src/data/dataProducts.js
@@ -2,24 +2,28 @@ import { beer, burger, fries, sushi } from '../assets/productImages';
 
 export default [
   {
+    id: 1,
     name: 'Tasty Burger',
     description: 'Australian bread, meat, cheddar, lettuce and pickles.',
     price: 32.90,
     img: burger,
   },
   {
+    id: 2,
     name: 'Fries',
     description: 'Tasty portion of crispy fries.',
     price: 11.00,
     img: fries,
   },
   {
+    id: 3,
     name: 'Sushi',
     description: 'Delights of japanese cuisine.',
     price: 39.00,
     img: sushi,
   },
   {
+    id: 4,
     name: 'Beer',
     description: 'Try one of the best beers.',
     price: 8.00,
