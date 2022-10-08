a8417d0755e324acbe1c8d006760725f2a49a2ae
Helder Medeiros <hmedeiros211@gmail.com>
2022-10-08 10:11:41 -0300

    feat(component): :sparkles: bump effect for cart button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Header.css';
+import AppContext from '../context/AppContext';
 
 function Header() {
+  const THREE_HUNDRED = 300;
+  const { cartQuantity } = useContext(AppContext);
+  const [bump, setBump] = useState(false);
+
+  useEffect(() => {
+    setBump(true);
+    const timer = setTimeout(() => {
+      setBump(false);
+    }, THREE_HUNDRED);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartQuantity]);
+
   return (
     <div className="header">
       <p className="header-title">FoodApp</p>
-      <button type="button" className="header-cart-button">
+      <button type="button" className={ `header-cart-button ${bump ? 'bump' : ''}` }>
         <span>Your Cart</span>
-        <span className="header-cart-quantity"> 0 </span>
+        <span className="header-cart-quantity">
+          { cartQuantity }
+        </span>
       </button>
     </div>
   );
